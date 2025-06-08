554da11724989b35f7569e19482bc02279360e08
edwin <edwin.lopez3@utp.ac.pa>
2025-06-08 16:10:24 -0500

    Se crearon RouterLinks para los componentes hijos. 1. Se modifico el componente side-menu-options para que solo tuviera las opciones de las rutas ya creadas. 2. Se importo en el index.html un toolkit de css. 3. Se crearon la interfaz necesario con su flujo de control para el side-menu.

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOptions {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent {}
+export class SideMenuOptionsComponent {
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ];
+}
